ee5e1f6181508f2e67ed71f3d366a9994181ff86
Emily Chou <e8chou@ucsd.edu>
2017-11-08 13:31:24 -0800

    Update DBHandler.java
    comments and clean up

--- app/src/main/java/com/ttmaps/maps/DBHandler.java
+++ app/src/main/java/com/ttmaps/maps/DBHandler.java
@@ -13,40 +13,44 @@ import android.util.Log;
  * Created by emilychou on 10/30/17.
  */
 
-    public class DBHandler extends SQLiteOpenHelper {
+public class DBHandler extends SQLiteOpenHelper {
 
-        private static final int DATABASE_VERSION = 3;
-        private static final String DATABASE_NAME = "poiInfo";
-        private static final String TABLE_POIS = "POIS";
-        private static final String KEY_ID = "id";
-        private static final String KEY_NAME = "name";
-        private static final int KEY_SH_DIST = 0;
+    private static final int DATABASE_VERSION = 3;
+    private static final String DATABASE_NAME = "poiInfo";
+    private static final String TABLE_POIS = "POIS";
+    private static final String KEY_ID = "id";
+    private static final String KEY_NAME = "name";
 
-        public DBHandler(Context context) {
-            super(context, DATABASE_NAME, null, DATABASE_VERSION);
-        }
-        @Override
-        public void onCreate(SQLiteDatabase db) {
-            Log.d("Creating: ","Creating..");
-            String CREATE_CONTACTS_TABLE = "CREATE TABLE IF NOT EXISTS " + TABLE_POIS + "(" + KEY_ID + " INTEGER PRIMARY KEY," + KEY_NAME + " TEXT NOT NULL UNIQUE" + ")";
-            db.execSQL(CREATE_CONTACTS_TABLE);
-        }
-        @Override
-        public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-            db.execSQL("DROP TABLE IF EXISTS " + TABLE_POIS);
-            onCreate(db);
-        }
+    public DBHandler(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+    
+    /* create the initial database */
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        Log.d("Creating: ","Creating..");
+        String CREATE_CONTACTS_TABLE = "CREATE TABLE IF NOT EXISTS " + TABLE_POIS + "(" + KEY_ID + " INTEGER PRIMARY KEY," + KEY_NAME + " TEXT NOT NULL UNIQUE" + ")";
+        db.execSQL(CREATE_CONTACTS_TABLE);
+    }
+    
+    /* drops table when database version is changed */
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_POIS);
+        onCreate(db);
+    }
 
+    /* add POI to the POI table */
     public void addPOI(POI poi) {
         SQLiteDatabase db = this.getWritableDatabase();
         ContentValues values = new ContentValues();
         values.put(KEY_ID, poi.getId());
-        values.put(KEY_NAME, poi.getName()); // POI Name
+        values.put(KEY_NAME, poi.getName());
         db.insertWithOnConflict(TABLE_POIS, null, values, SQLiteDatabase.CONFLICT_REPLACE);
-
         db.close();
     }
 
+    /* get a POI by id from the POI table */
     public POI getPOI(int id) {
         SQLiteDatabase db = this.getReadableDatabase();
         Cursor cursor = db.query(TABLE_POIS, new String[]{KEY_ID,
@@ -59,6 +63,7 @@ import android.util.Log;
         return contact;
     }
 
+    /* get a POI by name from the POI table */
     public POI getPOIByName(String name) {
         SQLiteDatabase db = this.getReadableDatabase();
         Cursor cursor = db.query(TABLE_POIS, new String[]{KEY_ID,
@@ -71,6 +76,7 @@ import android.util.Log;
         return contact;
     }
 
+    /* get a list of all POIs in the POI table */
     public List<POI> getAllPOIs() {
         List<POI> poiList = new ArrayList<POI>();
         String selectQuery = "SELECT * FROM " + TABLE_POIS;
@@ -87,6 +93,7 @@ import android.util.Log;
         return poiList;
     }
 
+    /* get the number of POIs currently in the table */
     public int getPOIsCount() {
         String countQuery = "SELECT * FROM " + TABLE_POIS;
         SQLiteDatabase db = this.getReadableDatabase();
@@ -96,6 +103,8 @@ import android.util.Log;
         return c;
     }
 
+    /* function to update a POI's fields (will edit in the future depending on
+       which fields we decide are constant and which are not */
     /*public int updatePOI(POI poi) {
         SQLiteDatabase db = this.getWritableDatabase();
 
@@ -106,6 +115,7 @@ import android.util.Log;
         new String[]{String.valueOf(poi.getId())});
     }*/
 
+    /* delete a POI from the POI table */
     public void deletePOI(POI poi) {
         SQLiteDatabase db = this.getWritableDatabase();
         db.delete(TABLE_POIS, KEY_ID + "=?",
