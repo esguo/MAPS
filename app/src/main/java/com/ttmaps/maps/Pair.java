b81d4e1e0465e0b596eb0087c84327d8f2b68bc1
Eddie <eddiesguo@gmail.com>
2017-11-27 22:26:55 -0800

    Added ability to filter paths.

--- app/src/main/java/com/ttmaps/maps/Pair.java
+++ app/src/main/java/com/ttmaps/maps/Pair.java
@@ -6,27 +6,31 @@ package com.ttmaps.maps;
 class Pair {
     private POI poi; //first member of pair
     private Edge edge; //second member of pair
+    private boolean lit;
+    private boolean safeBox;
+    private boolean stairs;
 
     Pair(POI first, Edge second) {
         poi = first;
         edge = second;
+
     }
 
 
-// --Commented out by Inspection START (11/19/2017 9:15 PM):
-//    public void setFirst(POI first) {
-//        poi = first;
-//    }
-// --Commented out by Inspection STOP (11/19/2017 9:15 PM)
-
-// --Commented out by Inspection START (11/19/2017 9:15 PM):
-//    public void setSecond(Edge second) {
-//        edge = second;
-//    }
-// --Commented out by Inspection STOP (11/19/2017 9:15 PM)
-
     POI getPOI() {
         return poi;
+    }
+
+    boolean isWellLit(){
+        return lit;
+    }
+
+    boolean hasSafeBox(){
+        return safeBox;
+    }
+
+    boolean noStairs(){
+        return !stairs;
     }
 
     Edge getEdge() {
