075fd61ec3d5e12c90b264a621965a298d669439
Eddie <eddiesguo@gmail.com>
2017-11-29 02:53:57 -0800

    Added pan/zoom function to floor plan image viewer

--- app/src/main/java/com/ttmaps/maps/FloorPlans.java
+++ app/src/main/java/com/ttmaps/maps/FloorPlans.java
@@ -2,46 +2,34 @@ package com.ttmaps.maps;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
-import android.widget.ArrayAdapter;
-import android.widget.AutoCompleteTextView;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.ImageView;
 
-//import static com.ttmaps.maps.DBHandler.poilist;
-//import static com.ttmaps.maps.MainActivity.POIs;
+
+import com.davemorrissey.labs.subscaleview.ImageSource;
+import com.davemorrissey.labs.subscaleview.SubsamplingScaleImageView;
+
+import static com.ttmaps.maps.MainActivity.POIList;
 
 /**
  * Created by Eddie on 11/28/2017.
  */
 
 public class FloorPlans extends AppCompatActivity{
-    private AutoCompleteTextView poiSearch;
-    private Button submitQuery;
-    private ImageView floorPlan;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.display_floorplan);
-        poiSearch = (AutoCompleteTextView) findViewById(R.id.search_floor_plan_textbox);
-        submitQuery = (Button) findViewById(R.id.submit_search_floorplan);
-        floorPlan = (ImageView) findViewById(R.id.imageView2);
 
-        DBHandler db = new DBHandler(FloorPlans.this);
-        for(POI poi: db.getPOIs()) {
-            //POIs.add(poi.getName());
+        SubsamplingScaleImageView imageView = (SubsamplingScaleImageView)findViewById(R.id.imageView);
+
+        String target = getIntent().getExtras().getString("floorPlan");
+
+        try {
+            int id = getResources().getIdentifier(POIList.get(target).getFileName(), "drawable", getPackageName());
+            imageView.setImage(ImageSource.resource(id));
         }
-        //ArrayAdapter<String> list = new ArrayAdapter<>(this, android.R.layout.select_dialog_singlechoice, POIs);
-        //poiSearch.setAdapter(list);
-
-        submitQuery.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick (View v){
-                String file = poiSearch.getText().toString();
-                floorPlan.setImageResource(getResources().getIdentifier(file, "drawable", getPackageName()));
-            }
-        });
+        catch (Exception e){
+            imageView.setImage(ImageSource.resource(R.drawable.no_plan));
+        }
     }
 }
