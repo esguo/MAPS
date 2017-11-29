ff67ade981002e0bc6cbd6107af1afe808f07611
Eddie <eddiesguo@gmail.com>
2017-11-29 02:58:20 -0800

    Added dynamic viewing of images based on POI name

--- app/src/main/java/com/ttmaps/maps/poi_info_window.java
+++ app/src/main/java/com/ttmaps/maps/poi_info_window.java
@@ -1,25 +1,49 @@
 package com.ttmaps.maps;
 
+import android.content.Intent;
+import android.graphics.drawable.Drawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
-import android.widget.Toast;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import static com.ttmaps.maps.MainActivity.POIList;
 
 public class poi_info_window extends AppCompatActivity implements View.OnClickListener {
+
+    String target;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_poi_info_window);
 
+        Bundle b = getIntent().getExtras();
+        target = b.getString("POI_Name");
+
         Button one = (Button) findViewById(R.id.go_button);
         one.setOnClickListener(this); // calling onClick() method
         ImageButton two = (ImageButton) findViewById(R.id.back_button);
         two.setOnClickListener(this);
         Button three = (Button) findViewById(R.id.floor_plans);
-        one.setOnClickListener(this); // calling onClick() method
+        three.setOnClickListener(this); // calling onClick() method
+        TextView POIName = (TextView) findViewById(R.id.poi_name);
+        POIName.setText(target);
+
+        /// get image and set to floorplan
+        try {
+            ImageView floorPlan = (ImageView) findViewById(R.id.loc_pic);
+            int id = getResources().getIdentifier(POIList.get(target).getFileName(), "drawable", getPackageName());
+            Drawable drawable = getResources().getDrawable(id);
+            floorPlan.setImageDrawable(drawable);
+        }
+        catch (Exception e){
+
+        }
     }
 
     // Button options on the info screen
@@ -28,13 +52,19 @@ public class poi_info_window extends AppCompatActivity implements View.OnClickLi
         switch (v.getId()) {
 
             case R.id.go_button:
-                Toast.makeText(poi_info_window.this, " In progess",
-                        Toast.LENGTH_LONG).show();
+                Intent intent = new Intent(this, MainActivity.class);
+                intent.putExtra("result", target);
+
+                setResult(RESULT_OK, intent);
+                finish();
                 break;
 
             case R.id.floor_plans:
-                Toast.makeText(poi_info_window.this, "Working on it",
-                        Toast.LENGTH_LONG).show();
+                Intent intent1 = new Intent(this, FloorPlans.class);
+                intent1.putExtra("floorPlan", target);
+
+
+                startActivity(intent1);
                 break;
 
             case R.id.back_button:
