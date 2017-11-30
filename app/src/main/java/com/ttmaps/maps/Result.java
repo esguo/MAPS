1e38482be5affcd4f2fd34ff7c3fbed0d73a35fa
Gabriel Ang <gang@ucsd.edu>
2017-11-29 23:20:58 -0800

    pretty sure i just made some graphic changes but forgot to push

--- app/src/main/java/com/ttmaps/maps/Result.java
+++ app/src/main/java/com/ttmaps/maps/Result.java
@@ -1,15 +1,24 @@
 package com.ttmaps.maps;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class Result extends AppCompatActivity {
+
+    private Button btn_viewmap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.results);
+
+        btn_viewmap = (Button) findViewById(R.id.viewMapBtn);
 
         Bundle bundle = getIntent().getExtras();
         String r;
@@ -17,6 +26,16 @@ public class Result extends AppCompatActivity {
 
         TextView result = (TextView) findViewById(R.id.result);
         result.setText(r);
+
+        final Context context = this;
+        btn_viewmap.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent intent;
+                intent = new Intent(context, MapsActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 
 }
